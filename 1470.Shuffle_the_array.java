240c03a60d5a207c558e7a27648122aaba85bdbb
PrasiddhShah <prasiddhshah.18@gmail.com>
2024-08-17 13:10:12 -0400

    Create 1470.Shuffle_the_array.java

---
+++ 1470.Shuffle_the_array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int j=0;
+        int arr[]=new int[nums.length];
+        for(int i=0;i<n;i++){
+            arr[j]=nums[i];
+            arr[++j]=nums[i+n];
+            j++;
+        }
+           return arr;
+
+    }
+}
